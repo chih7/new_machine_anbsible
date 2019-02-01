49c79c28b8174689582ad111ef87c650d9b6e1f8
chih <i@chih.me>
2019-02-01 22:32:53 +0800

    add mariadb and nextcloud install

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-ansible-playbook -i envs/la/hosts ./playbooks/new_machine.yaml -vvv 
+ansible-playbook -i envs/la/hosts ./playbooks/new_machine.yaml -vvv --extra-vars "mariadb_root_password=root_password"
